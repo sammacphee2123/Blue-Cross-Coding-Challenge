3c3e68e58d1b5626677a252ab77aa8f932b154f0
sammacphee2123 <samueljmacphee@gmail.com>
2023-03-06 15:34:42 -0400

    Fixed navigation bugs

--- blue-cross-coding-challenge/src/Pages/RecipeDetails.js
+++ blue-cross-coding-challenge/src/Pages/RecipeDetails.js
@@ -2,11 +2,14 @@ import { useState, useEffect} from 'react';
 import { useParams, } from "react-router-dom";
 import Button from '@mui/material/Button';
 import Grid from '@mui/material/Grid';
+import { Link } from "react-router-dom";
+import { useNavigate } from 'react-router'
 
 const RecipeDetails = () => {
     const [details, setDetails] = useState([]);
-    const [healthInfo, setHealthInfo] = useState([]);
+    const healthInfo = ["No", "No", "No", "No"];
     const params = useParams();//The selected recipe to query for details.
+    const navigate = useNavigate()
 
     //Query the spoonacular API to GET all details regarding the selected recipe.
     const fetchDetails = async () => {
@@ -23,7 +26,7 @@ const RecipeDetails = () => {
       fetchDetails().then((data) => {
         if (isMounted) {
           setDetails(data);
-          console.log(data)
+          console.log(data);
           storeHealthInfo();
         }
       });
@@ -34,29 +37,31 @@ const RecipeDetails = () => {
 
     const storeHealthInfo = () => {
       if (details.vegan) {
-        setHealthInfo(healthInfo + "Vegan ");
+        healthInfo[0] = ("Yes");
       }
       if (details.vegetarian) {
-        setHealthInfo(healthInfo + "Vegetarian ");
+        healthInfo[1] = ("Yes");
       }
       if (details.dairyFree) {
-        setHealthInfo(healthInfo + "Dairy Free ");
+        healthInfo[2] = ("Yes");
       }
       if(details.glutenFree) {
-        setHealthInfo(healthInfo + "Gluten Free ");
+        healthInfo[3] = ("Yes");
       }
     }
 
-    const nextPath = (path) => {
-      this.props.history.push(path);
+    const goHome = () => {
+      navigate('/');
+      navigate(0);
     }
 
     return(
         <Grid container>
           <Grid item xs={6} paddingTop={5} paddingLeft={5}>
-            <Button variant="contained" onClick={() => this.nextPath('/')}>Home</Button>
+            <Button variant="contained" onClick={() => goHome()}>Home</Button>
+            
             <h1>{details.title}</h1>
-            <h2>Health Information: {healthInfo}</h2>
+            <h2>Health Information:</h2>
             <h3>Vegan: {healthInfo[0]}</h3>
             <h3>Vegetarian: {healthInfo[1]}</h3>
             <h3>Dairy Free: {healthInfo[2]}</h3>
@@ -68,17 +73,16 @@ const RecipeDetails = () => {
           
           <Grid item xs={12} paddingLeft={5} paddingRight={5} paddingBottom={5}>
             <h2>Ingredients: </h2>
+            {/* This is causing the appication to crash due to details being undefined
             <ul>
               {details.extendedIngredients.map(({ id, original }) => (
                 <li key={id}>{original}</li>
               ))}
             </ul>
+            */}
             <h2>Instructions: </h2>
             <p dangerouslySetInnerHTML={{ __html: details.instructions }}></p>
-          </Grid>
-
-          
-          
+          </Grid>   
         </Grid>       
     )
 }
